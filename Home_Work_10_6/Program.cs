0fb7c0dd48baacc6a7f7cbf1891a60f2de9e394d
Владислав Павлов <vlpavlov58@gmail.com>
2018-03-12 23:32:48 +0200

    code

--- Home_Work_10_6/Program.cs
+++ Home_Work_10_6/Program.cs
@@ -8,8 +8,48 @@ namespace Home_Work_10_6
 {
     class Program
     {
+        static int MaxElement(int x)
+        {
+            int[] array = { 1, 5, 69, 78, 2, 37, 0, 7, 3 };
+            int max = array[0];
+            
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (max < array[i])
+                {
+                    max = array[i];
+                }
+                
+            }
+            return max;
+            
+        }
+
+        static int MinElement (int x)
+        {
+            int[] array = { 1, 5, 69, 78, 2, 37, 0, 7, 3 };
+            int min = array[0];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                
+                if (min > array[i])
+                {
+                    min = array[i];
+                }
+            }
+            return min;
+            
+            
+        }
+
         static void Main(string[] args)
         {
+            int[] array = { 1, 5, 69, 78, 2, 37, 0, 7, 3 };
+            int max = array[0];
+            int min = array[0];
+            Console.WriteLine($"Max Element: = {MaxElement(max)}");
+            Console.WriteLine($"Min Element: = {MinElement(min)}");
         }
     }
 }
