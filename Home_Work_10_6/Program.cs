64ff90147451a38df4193d6f0c90cbef651e459e
Владислав Павлов <vlpavlov58@gmail.com>
2018-03-12 23:12:25 +0200

    Add project files.

---
+++ Home_Work_10_6/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Work_10_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
